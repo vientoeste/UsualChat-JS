cfd2d75969231775c707e7cff990a0082801639a
djjd2000 <djjd2000@naver.com>
2022-05-03 14:55:21 +0900

    node v.17.x 이상에서 mongodb 접속 오류 해결

--- schemas/index.js
+++ schemas/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const { MONGO_ID, MONGO_PASSWORD, NODE_ENV } = process.env;
-const MONGO_URL = `mongodb://${MONGO_ID}:${MONGO_PASSWORD}@localhost:27017/admin`;
+const MONGO_URL = `mongodb://${MONGO_ID}:${MONGO_PASSWORD}@127.0.0.1:27017/admin`;
 console.log(MONGO_URL);
 const connect = () => {
   if (NODE_ENV !== 'production') {
