ddc30948a3f6648b6d812fd27d4e835e02144853
djjd2000 <djjd2000@naver.com>
2022-05-03 12:25:14 +0900

    일부 자동화

--- automation.js
+++ automation.js
@@ -1,10 +1,49 @@
 const fs = require('fs');
 const path = require('path');
+const readline = require('readline');
 
-console.log(path.dirname(__filename));
-let string = `cd ${path.dirname(__filename)}
+function nodemonapp() {
+    //Windows 환경에서 nodemon으로 서버 실행하는 batch파일 생성
+    let string =
+`cd ${path.dirname(__filename)}
 nodemon app`;
-fs.appendFile('launchServer.bat', string, err => {
-    if(err) throw err;
-    console.log(`complete`);
-})
+    fs.appendFile('launchServer.bat', string, err => {
+        if(err) throw err;
+        console.log(`nodemon app complete`);
+    })
+}
+nodemonapp();
+
+function mongo(ver) {
+//test용 mongodb 실행
+    let string1 = `cd C:\\program files\\mongodb\\server\\${ver}\\bin
+mongod`
+    let string2 = `cd C:\\program files\\mongodb\\server\\${ver}\\bin
+mongo`
+    fs.appendFile('mongod.bat', string1, err => {
+        if (err)
+            throw err;
+        console.log(`mongod complete`);
+    })
+    fs.appendFile('mongo.bat', string2, err => {
+        if (err)
+            throw err;
+        console.log(`mongo complete`);
+    })
+}
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout,
+// })
+// let dbver;
+// rl.on('line', async(line) => {
+//     dbver = line;       
+//     mongo(dbver);   
+//     console.log(`입력된 버전은 ${dbver}입니다.`, line);    
+//     rl.close();    
+ 
+// })
+// rl.on('close', () => {
+//     process.exit();
+// })
+mongo('5.0')
